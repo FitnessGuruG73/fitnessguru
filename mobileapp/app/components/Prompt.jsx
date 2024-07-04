0161a5968408414735765c7ab1b92f4dc38c6db7
srishmaalladi <srishmaalladi14@gmail.com>
2024-07-04 15:53:01 +0530

    frontend and backend

---
+++ mobileapp/app/components/Prompt.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+const Prompt = () => {
+  return (
+    <View calsss="bg-">
+      <Text className='text-red-600'>Prompt</Text>
+      <Text> hi </Text>
+    </View>
+  )
+}
+export default Prompt
