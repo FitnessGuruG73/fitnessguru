d20fab4874fd7e44602117624e10812c1091a38a
valli <vallivirat999@gmail.com>
2024-07-10 15:42:19 +0530

    refactored

--- backend/scripts/app.py
+++ backend/scripts/app.py
@@ -1,37 +1,31 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from routes.signup import signup_bp  # Ensure to import the mongo object
+from routes.signup import signup_bp 
 from routes.login import login_bp
 from routes.video import video_bp
 
-app = Flask(__name__)
 
-app.config.from_prefixed_env()
-mongo = PyMongo(app)
-app.config['pymongo'] = mongo
+def createApp():
+    app = Flask(__name__)
+    app.config.from_prefixed_env() #importing flask environmental variables to config object
+    mongo = PyMongo(app)
+    app.config['pymongo'] = mongo
+    CORS(app)  # Enable CORS for all routes 
 
-CORS(app)  # Enable CORS for all routes
+    # Register Blueprints
+    app.register_blueprint(signup_bp)
+    app.register_blueprint(login_bp)
+    app.register_blueprint(video_bp)
+    return app
 
-# mongo_uri = "mongodb+srv://srishmaalladi14:8Vp7FYYqSLVXBAMw@cluster0.jtgcpyd.mongodb.net/Videos?retryWrites=true&w=majority&appName=Cluster0"
-# Configure the MongoDB URI
-# app.config["MONGO_URI"] = mongo_uri
-
-# # Initialize PyMongo with the Flask app
-# mongo.init_app(app)
+app = createApp() 
 
 # Define a route for the home page
 @app.route('/')
 def home():
     return "Hello, Flask with MongoDB!"
 
-# Register Blueprints
-app.register_blueprint(signup_bp)
-app.register_blueprint(login_bp)
-app.register_blueprint(video_bp)
-
-
-
-# if __name__ == '__main__':
-#     # Run the Flask app in debug mode
-#     app.run(host='0.0.0.0' ,debug=True,port=5000)
+if __name__ == '__main__':
+    # Run the Flask app in debug mode
+    app.run(host='0.0.0.0' ,debug=True,port=5500)
--- backend/scripts/routes/login.py
+++ backend/scripts/routes/login.py
@@ -11,7 +11,6 @@ mongo = PyMongo()
 login_bp = Blueprint('login', __name__) 
 
 # Get secret key from environment variable
-SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
 
 @login_bp.route('/login', methods=['POST'])
 def login():
@@ -24,6 +23,7 @@ def login():
     user = mongo.db.Users.find_one({'username': username})
     
     if user and check_password_hash(user['password'], password): 
+        SECRET_KEY = current_app.config['SECRET_KEY']
         # Password verification successful, generate JWT
         expiration_time = datetime.utcnow() + timedelta(hours=2)
         access_token = jwt.encode({
